78b9e25613b711a7a21e5d429cd160fc9c8a7b0a
Thaís Silva <thaiscaroline.cms@gmail.com>
2022-03-23 17:54:01 -0300

    hamburguer menu

--- src/components/topbar/Topbar.jsx
+++ src/components/topbar/Topbar.jsx
@@ -1,12 +1,14 @@
 import "./topbar.scss";
 import { Person, Mail } from "@material-ui/icons";
 
-export default function Topbar() {
+export default function Topbar({menuOpen, setMenuOpen}) {
   return (
-    <div className="topbar">
+    <div className={"topbar " + (menuOpen && "active")} >
       <div className="wrapper">
         <div className="left">
-          <a href="#intro" className="logo">oi</a>
+          <a href="#intro" className="logo">
+            oi
+          </a>
           <div className="itemContainer">
             <Person className="icon"/>
             <span>+55 31 0000-0000</span>
@@ -17,7 +19,7 @@ export default function Topbar() {
           </div>
         </div>
         <div className="right">
-          <div className="hamburger">
+          <div className="hamburger" onClick={() => setMenuOpen(!menuOpen)}>
             <span className="line1"></span>
             <span className="line2"></span>
             <span className="line3"></span>
